e0f135549a950f31d6afaa4dc13763a36d8f5918
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-08-29 10:58:42 +0000

    Added Archive object
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/trunk/tue_serialization@26829 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- test/test_serialization.cpp
+++ test/test_serialization.cpp
@@ -1,3 +1,4 @@
+#include <tue/serialization/archive.h>
 #include <tue/serialization/input_archive.h>
 #include <tue/serialization/output_archive.h>
 
@@ -36,6 +37,30 @@ int main(int argc, char **argv) {
         tue::serialization::InputArchive a_in(f_in);
         a_in >> d >> f >> i >> s;
         f_in.close();
+
+        std::cout << d << ", " << f << ", " << i << ", \"" << s << "\"" << std::endl;
+    }
+
+    tue::serialization::Archive a;
+
+    {
+        // example
+        double d = 3.15;
+        float f = 5.0;
+        int i = 123;
+        std::string s = "Hello, this is just a simple test";
+
+        a << d << f << i << s;
+    }
+
+    {
+        // read
+        double d;
+        float f;
+        int i;
+        std::string s;
+
+        a >> d >> f >> i >> s;
 
         std::cout << d << ", " << f << ", " << i << ", \"" << s << "\"" << std::endl;
     }
